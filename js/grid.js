39847284761fe356df68d6221ccb04f11c1e1e15
Marco Venter <marcov@bbd.co.za>
2022-05-18 13:24:09 +0200

    score top/bottom scroll

--- js/grid.js
+++ js/grid.js
@@ -18,7 +18,7 @@ let timerID = -1;
 
 
 function showGrid() {
-  populateGrid(sessionStorage.getItem("gridX"),sessionStorage.getItem("gridY"));
+  populateGrid(sessionStorage.getItem("gridX"), sessionStorage.getItem("gridY"));
 }
 
 function playAgain() {
@@ -157,7 +157,7 @@ function resetBoard() {
   });
 })();
 
-const cardImages = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50'];
+const cardImages = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50'];
 
 function randomCardImages(gridSizeX, gridSizeY) {
   let randomSubset = cardImages.map(img => [img, Math.random()]).sort((a, b) => {
--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,99 +1,191 @@
-const leaderboardData = [   { rank: 1, name: "Bob", moves: "***", time:"***", points:"***"},
-                            { rank: 2, name: "George", moves: "***", time:"***", points:"***"},
-                            { rank: 3, name: "Kevin", moves: "***", time:"***", points:"***"},
-                            { rank: 4, name: "Alice", moves: "***", time:"***", points:"***"},
-                            { rank: 5, name: "John", moves: "***", time:"***", points:"***"},
-                            { rank: 6, name: "Marco", moves: "***", time:"***", points:"***"},
-                            { rank: 7, name: "Marie", moves: "***", time:"***", points:"***"},
-                            { rank: 8, name: "Luan", moves: "***", time:"***", points:"***"},
-                            { rank: 9, name: "Tiaan", moves: "***", time:"***", points:"***"},
-                            { rank: 10, name: "Sandy", moves: "***", time:"***", points:"***"},
-                            { rank: 11, name: "Bob", moves: "***", time:"***", points:"***"},
-                            { rank: 12, name: "George", moves: "***", time:"***", points:"***"},
-                            { rank: 13, name: "Kevin", moves: "***", time:"***", points:"***"},
-                            { rank: 14, name: "Alice", moves: "***", time:"***", points:"***"},
-                            { rank: 15, name: "John", moves: "***", time:"***", points:"***"},
-                            { rank: 16, name: "Marco", moves: "***", time:"***", points:"***"},
-                            { rank: 17, name: "Marie", moves: "***", time:"***", points:"***"},
-                            { rank: 18, name: "Luan", moves: "***", time:"***", points:"***"},
-                            { rank: 19, name: "Tiaan", moves: "***", time:"***", points:"***"},
-                            { rank: 20, name: "Sandy", moves: "***", time:"***", points:"***"},
-                            { rank: 21, name: "Bob", moves: "***", time:"***", points:"***"},
-                            { rank: 22, name: "George", moves: "***", time:"***", points:"***"},
-                            { rank: 23, name: "Kevin", moves: "***", time:"***", points:"***"},
-                            { rank: 24, name: "Alice", moves: "***", time:"***", points:"***"},
-                            { rank: 25, name: "John", moves: "***", time:"***", points:"***"},
-                            { rank: 26, name: "Marco", moves: "***", time:"***", points:"***"},
-                            { rank: 27, name: "Marie", moves: "***", time:"***", points:"***"},
-                            { rank: 28, name: "Luan", moves: "***", time:"***", points:"***"},
-                            { rank: 29, name: "Tiaan", moves: "***", time:"***", points:"***"},
-                            { rank: 30, name: "Sandy", moves: "***", time:"***", points:"***"},
-                            { rank: 31, name: "Bob", moves: "***", time:"***", points:"***"},
-                            { rank: 32, name: "George", moves: "***", time:"***", points:"***"},
-                            { rank: 33, name: "Kevin", moves: "***", time:"***", points:"***"},
-                            { rank: 34, name: "Alice", moves: "***", time:"***", points:"***"},
-                            { rank: 35, name: "John", moves: "***", time:"***", points:"***"},
-                            { rank: 36, name: "Marco", moves: "***", time:"***", points:"***"},
-                            { rank: 37, name: "Marie", moves: "***", time:"***", points:"***"},
-                            { rank: 38, name: "Luan", moves: "***", time:"***", points:"***"},
-                            { rank: 39, name: "Tiaan", moves: "***", time:"***", points:"***"},
-                            { rank: 40, name: "Sandy", moves: "***", time:"***", points:"***"},
-                            { rank: 41, name: "Bob", moves: "***", time:"***", points:"***"},
-                            { rank: 42, name: "George", moves: "***", time:"***", points:"***"},
-                            { rank: 43, name: "Kevin", moves: "***", time:"***", points:"***"},
-                            { rank: 44, name: "Alice", moves: "***", time:"***", points:"***"},
-                            { rank: 45, name: "John", moves: "***", time:"***", points:"***"},
-                            { rank: 46, name: "Carmen", moves: "***", time:"***", points:"***"},
-                            { rank: 47, name: "Marie", moves: "***", time:"***", points:"***"},
-                            { rank: 48, name: "Luan", moves: "***", time:"***", points:"***"},
-                            { rank: 50, name: "Tiaan", moves: "***", time:"***", points:"***"}
-                        ];
+// handles requests to server
+async function sendRequest(url, type) {
+    const options = {
+        method: `${type}`,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
 
+    // async request -> server
+    let response_obj = await fetch(url, options)
+        .catch(e => {
+            // failure: wrap error
+            return serverError();
+        });
 
-const tableHeaders = ['rank' , 'name', 'moves', 'time', 'points'];
+    // optional chaining catch used when query is unsucessful (like ERR_CONNECTION_REFUSED)
+    try {
+        response_obj = await response_obj.json();
+    } finally {
+        return response_obj;
+    }
+}
+let usersInfo = "";
+// get all a grid's info with gridID = [4x4 = 1], [6x6 = 2], [8x8 = 3]
+async function getScores(gridID) {
 
-let activePlayerName = 'Carmen';
+    usersInfo = await sendRequest(`http://localhost:3000/api/scores?gridID=${gridID}`, 'GET');
+    printQueryFeedback(`SERVER: get scores response sucess`, usersInfo);
 
-function populateLeaderboard(){
-    /***
-     * TODO: API call here for getting top 100 
-     */
-    const tableBody = document.getElementById("leaderBoardResults");
+    return usersInfo;
 
-     leaderboardData.forEach((leader)=>{
-        let leaderRow = document.createElement('tr') ;
+}
 
-        if(leader.name === activePlayerName){
-            let tableFoot = document.getElementById('activePlayerResults') ;
-            let footerRow = document.createElement('tr') ; 
+function printQueryFeedback(header, responseObj) {
+    if (!responseObj.success) {
+        console.log(`${header}: false... error: ${JSON.stringify(responseObj.error, null, 2)}`);
+    } else {
+        console.log(`${header}: true!`);
+    }
+}
 
+function serverError() {
+    return {
+        success: false,
+        error: "server not running... try again"
+    };
+}
+
+const tableHeaders = ['rank', 'username', 'moves', 'time_', 'score'];
+
+let activePlayerName = 'JJJ';
+let activePlayerMoves = 12;
+let activePlayerTime = 33;
+let activePlayerScore = 10481;
+
+function between(x, min, max) {
+    return x >= min && x <= max;
+}
+let previousY = 0;
+let currentY = 0;
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+
+        // update 
+        if (entry.target.classList.value === "activeFontColor") {
+            currentY = entry.boundingClientRect.y
         }
 
-        tableHeaders.forEach((tableProperty) =>{
-            let leaderColumn = document.createElement('td');
-            leaderColumn.innerHTML = leader[tableProperty];
-            
-            if(leader.name === activePlayerName){
-                let tableFoot = document.getElementById('activePlayerResults') ;
-                let footerData = document.createElement('td') ; 
-                footerData.innerHTML = leader[tableProperty];
+        if (entry.target.classList.value === "activeFontColor") {
 
-                if(tableProperty != 'rank' && tableProperty != 'name'){
-                    footerData.setAttribute('class', 'hideColumn') ; 
+            if (entry.isIntersecting) {
+                // showing (hide header/footer)
+                document.getElementById("activePlayerResults-footer").style.color = "#FFFFFF";
+                document.getElementById("activePlayerResults-header").style.color = "#FFFFFF";
+            } else {
+                if (currentY <= previousY && previousY !== 0) {
+                    // show header
+                    document.getElementById("activePlayerResults-footer").style.color = "#FFFFFF";
+                    document.getElementById("activePlayerResults-header").style.color = "red";
+                } else {
+                    // show footer
+                    document.getElementById("activePlayerResults-footer").style.color = "red";
+                    document.getElementById("activePlayerResults-header").style.color = "#FFFFFF";
                 }
+            }
+        }
+        previousY = currentY;
+    })
+}, {
+    threshold: [0.5]
+});
 
-                tableFoot.appendChild(footerData) ;
+async function populateLeaderboard() {
+    // get top 100 scores for a grid
+    let responseBody = await getScores(1);
+
+    // add player score
+    let curPlayerData = {
+        score: activePlayerScore,
+        username: activePlayerName,
+        moves: activePlayerMoves,
+        time_: activePlayerTime
+    };
+
+    responseBody.data.push(curPlayerData);
+
+    // sort descending
+    let leaderboardData = responseBody.data.sort((a, b) => {
+        // compare logic
+        if (a.score > b.score) return -1;
+        if (a.score < b.score) return 1;
+        return 0;
+    });
+
+    // only take top 100
+    leaderboardData.data = responseBody.data.slice(0, 100);
+
+    const tableBody = document.getElementById("leaderBoardResults");
+
+    let passedHighest = false;
+    leaderboardData.data.forEach((leader, index) => {
+
+        let leaderRow = document.createElement('tr');
+
+        // add observer
+        observer.observe(leaderRow);
+
+        tableHeaders.forEach((tableProperty) => {
+
+            let leaderColumn = document.createElement('td');
+
+            // cur player
+            if (leader.username === activePlayerName) {
+                leaderRow.setAttribute('class', 'activeFontColor');
+            } else {
+                if (passedHighest) {
+                    leaderRow.setAttribute('class', 'score-lower');
+                } else {
+                    leaderRow.setAttribute('class', 'score-higher');
+                }
             }
 
+            // other players
+            if (tableProperty === 'rank') {
+                leaderColumn.innerHTML = index + 1;
+            } else {
+                leaderColumn.innerHTML = leader[tableProperty];
+            }
 
-            if(tableProperty != 'rank' && tableProperty != 'name'){
-                leaderColumn.setAttribute('class', 'hideColumn') ; 
+            // cur player
+            if (leader.username === activePlayerName) {
+                let tableFoot = document.getElementById('activePlayerResults-footer');
+                let tableHead = document.getElementById('activePlayerResults-header');
+
+                let footerData = document.createElement('td');
+                let headerData = document.createElement('td');
+
+                if (tableProperty === 'rank') {
+                    footerData.innerHTML = index + 1;
+                    headerData.innerHTML = index + 1;
+                } else {
+                    footerData.innerHTML = leader[tableProperty];
+                    headerData.innerHTML = leader[tableProperty];
+                }
+
+                if (tableProperty != 'rank' && tableProperty != 'username') {
+
+                    footerData.setAttribute('class', 'hideColumn');
+                    headerData.setAttribute('class', 'hideColumn');
+                }
+
+                tableFoot.appendChild(footerData);
+                tableHead.appendChild(headerData);
+            }
+
+            if (tableProperty != 'rank' && tableProperty != 'username') {
+                leaderColumn.setAttribute('class', 'hideColumn');
+            }
+
+            if (leader.username === activePlayerName) {
+                passedHighest = true;
             }
 
             leaderRow.appendChild(leaderColumn);
         });
 
         tableBody.appendChild(leaderRow);
-     }); 
-    
+    });
+
 }
