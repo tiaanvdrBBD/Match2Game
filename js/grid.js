8b709a826d2a1bc66aaeef829638bb7eda957f2b
Tiaan van der Riel <tiaanvdr@bbd.co.za>
2022-05-19 14:37:36 +0200

    fixed moves counter

--- js/grid.js
+++ js/grid.js
@@ -62,7 +62,7 @@ function tick() {
       showGameOver();
       document.getElementById("heading").innerHTML = `Oops!`;
       document.getElementById("gameOverMessage").innerHTML = `You were caught...`;
-      document.getElementById("moves").innerHTML = `Moves: ${successful + unsuccessful}`;
+      document.getElementById("moves").innerHTML = `Moves: ${moves}`;
       document.getElementById("time").innerHTML = `Time: ${minutes + "m " + seconds + "s"}`;
       document.getElementById("gameOverImg").src = `../img/unsuccess.svg`;
     }
@@ -111,7 +111,7 @@ function flipCard() {
     document.getElementById("heading").innerHTML = `Congratulations!`;
     document.getElementById("gameOverMessage").innerHTML = `You made it out the fish pond.`;
     document.getElementById("score").innerHTML = `${evaluation()} - ${sessionStorage.getItem('username')} `;
-    document.getElementById("moves").innerHTML = `Moves: ${successful + unsuccessful}`;
+    document.getElementById("moves").innerHTML = `Moves: ${moves}`;
     document.getElementById("time").innerHTML = `Time: ${minutes + "m " + seconds + "s"}`;
     document.getElementById("gameOverImg").src = `../img/success.svg`;    // reset everything
     
@@ -131,7 +131,7 @@ function flipCard() {
     showGameOver();
     document.getElementById("heading").innerHTML = `Oops!`;
     document.getElementById("gameOverMessage").innerHTML = `You were caught...`;
-    document.getElementById("moves").innerHTML = `Moves: ${successful + unsuccessful}`;
+    document.getElementById("moves").innerHTML = `Moves: ${moves}`;
     document.getElementById("time").innerHTML = `Time: ${minutes + "m " + seconds + "s"}`;
     document.getElementById("gameOverImg").src = `../img/unsuccess.svg`;
   } else {
